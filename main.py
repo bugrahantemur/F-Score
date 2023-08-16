8826a206ea8809aeccb4547527306ef14bd43a3b
Bugrahan Temuer <bugrahan.temur@gmail.com>
2023-08-17 00:21:55 +0200

    Rewrite comments

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import pandas as pd
 from fscore import f_score
 
 if __name__ == "__main__":
-    # Read the data as csv and create a pandas DataFrame
+    # Create a data-frame containing the fundamental data of firms
     data = pd.read_csv("./data/fundamentals.csv", index_col=0)
 
-    # Set the year of calculation for Piotroski F-Score
+    # Specify the considered year to find the F-Score
     year = 2022
 
-    # Get new field "F-Score"
+    # Calculate the F-Score and put into the data frame
     data["F-Score"] = f_score(data, year)
 
     # Print the "F-Score" column to the console
