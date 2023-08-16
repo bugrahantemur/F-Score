111b274be95cf7ee184cf7adcd8bedc18c9d9d35
Bugrahan Temuer <bugrahan.temur@gmail.com>
2023-08-17 00:09:09 +0200

    Add name eq main

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import pandas as pd
 from fscore import f_score
 
-# Read the data as csv and create a pandas DataFrame
-data = pd.read_csv("./data/company_data.csv", index_col=0)
+if __name__ == "__main__":
+    # Read the data as csv and create a pandas DataFrame
+    data = pd.read_csv("./data/company_data.csv", index_col=0)
 
-# Set the year of calculation for Piotroski F-Score
-year = 2022
+    # Set the year of calculation for Piotroski F-Score
+    year = 2022
 
-# Get new field "F-Score"
-data["F-Score"] = f_score(data, year)
+    # Get new field "F-Score"
+    data["F-Score"] = f_score(data, year)
 
-# Print the "F-Score" column to the console
-print(data.to_string(columns=["F-Score"]))
+    # Print the "F-Score" column to the console
+    print(data.to_string(columns=["F-Score"]))
